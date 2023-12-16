fd5505096ca28926e495214f447ad6eebe0313df
koushirou00 <130426310+koushirou00@users.noreply.github.com>
2023-12-16 14:24:15 +0900

    フィードバック内容の修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react"
 import { Routes, Route } from 'react-router-dom';
 
 // コンポーネント
@@ -7,24 +6,11 @@ import PostList from "components/PostList";
 import Post from "routes/post"
 
 function App() {
-  const [posts, setPosts] = useState(null)
-  useEffect(() => {
-    (async function () {
-      try {
-        const fetchData = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
-        const json = await fetchData.json()
-        setPosts(json.posts)
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, [])
-
   return (
     <div className="text-gray-800 text-base">
       <Header />
       <Routes>
-        <Route path="/" element={<PostList posts={posts} />} />
+        <Route path="/" element={<PostList />} />
         <Route path="/posts/:id" element={<Post />} />
       </Routes>
     </div>
--- src/routes/post.jsx
+++ src/routes/post.jsx
@@ -4,7 +4,6 @@ import HTMLReactParser from 'html-react-parser'
 
 import Loading from 'components/Loading';
 
-
 const Post = () => {
   const [post, setPost] = useState(null);
   const params = useParams();
@@ -21,28 +20,26 @@ const Post = () => {
     }
   }, [params.id])
 
+  if (!post) return <Loading />
+
   return (
-    post ? (
-      <div className='mt-11 mx-auto p-3 max-w-[800px]'>
-        <div >
-          <img src="https://placehold.jp/800x400.png" alt="" />
-        </div>
-        <div className='p-2'>
-          <div className='flex justify-between mt-3'>
-            <div className='text-sm'>{new Date(post.createdAt).toLocaleDateString()}</div>
-            <div className='flex px-5'>
-              {post.categories.map((category) => (
-                <div key={category} className='px-2 py-1.5 mx-1 border border-blue-500 rounded text-blue-500 text-xs'>{category}</div>
-              ))}
-            </div>
-          </div>
-          <h2 className='text-2xl py-3'>{post.title}</h2>
-          <div className='leading-normal'>{HTMLReactParser(post.content)}</div>
-        </div>
+    <div className=' mt-11 mx-auto p-3 max-w-[800px]'>
+      <div >
+        <img src={post.thumbnailUrl} alt="" />
       </div>
-    ) : (
-      <Loading />
-    )
+      <div className='p-2'>
+        <div className='flex justify-between mt-3'>
+          <div className='text-sm'>{new Date(post.createdAt).toLocaleDateString()}</div>
+          <div className='flex px-5'>
+            {post.categories.map((category) => (
+              <div key={category} className='px-2 py-1.5 mx-1 border border-blue-500 rounded text-blue-500 text-xs'>{category}</div>
+            ))}
+          </div>
+        </div>
+        <h2 className='text-2xl py-3'>{post.title}</h2>
+        <div className='leading-normal'>{HTMLReactParser(post.content)}</div>
+      </div>
+    </div>
   )
 }
 
